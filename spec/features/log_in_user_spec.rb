bfeb5564de9e97988df8799a4df92af5843316e4
Jimmy Thelander <jimmy.thelander@gmail.com>
2015-02-01 13:35:31 +0100

    Implement log in user feature test

--- spec/features/log_in_user_spec.rb
+++ spec/features/log_in_user_spec.rb
@@ -1,12 +1,29 @@
 feature "log in user" do
   before do
+    User.create email: "test@mail.com", password: "password"
+
     visit root_path
   end
 
   scenario "with correct credentials" do
-    click_link 
+    click_link "Logga in"
+    within ".session" do
+      fill_in "user_email", with: "test@mail.com"
+      fill_in "user_password", with: "password"
+      click_button "Logga in"
+    end
+
+    expect(page).to have_content "Signed in successfully"
   end
 
   scenario "with incorrect credentials" do
+    click_link "Logga in"
+    within ".session" do
+      fill_in "user_email", with: "test@mail.com"
+      fill_in "user_password", with: "wordpass"
+      click_button "Logga in"
+    end
+
+    expect(page).to have_content "Invalid email or password"
   end
 end
