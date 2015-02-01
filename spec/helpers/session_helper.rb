469eb13db14caa53c29bb131f8e8d08c362e2223
Jimmy Thelander <jimmy.thelander@gmail.com>
2015-02-01 13:35:42 +0100

    Add test helpers

---
+++ spec/helpers/session_helper.rb
@@ -0,0 +1,12 @@
+module SessionHelper
+  def login user
+    visit root_path
+
+    click_link "Logga in"
+    within ".session" do
+      fill_in "user_email", with: user.email
+      fill_in "user_password", with: user.password
+      click_button "Logga in"
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,13 +3,16 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/helpers/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # Include helpers
+  config.include SessionHelper
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
