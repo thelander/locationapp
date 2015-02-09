8f494b59d32cadb3715e16cf484bc5e86845ece2
Jimmy Thelander <jimmy.thelander@gmail.com>
2015-02-09 16:51:46 +0100

    Fix market rating routes and javascript

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resource :profile, only: :show
   resources :markets, path: "m" do
-    resources :comments, only: [:create, :destroy]
-    resources :ratings, only: :update
+    resources :comments, path: "c", only: [:create, :destroy]
+    resources :ratings, path: "r", only: :update
   end
 end
