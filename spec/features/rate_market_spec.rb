05a66df4342ecdde5e4889407b0fd0e1728b761e
Jimmy Thelander <jimmy.thelander@gmail.com>
2015-02-10 16:49:08 +0100

    Implement rate market feature tests

---
+++ spec/features/rate_market_spec.rb
@@ -0,0 +1,23 @@
+feature "rate market" do
+  before do
+    user = User.create email: "test@mail.com", password: "password"
+    market = Market.create name: "Test market", description: "Description of test market"
+
+    login user
+    visit market_path market
+  end
+
+  scenario "with a poor score", js: true do
+    find("#user-rating > img:nth-child(1)").click
+    reload
+
+    expect(page).to have_selector "#average-rating img[title='bad']"
+  end
+
+  scenario "with a good score", js: true do
+    find("#user-rating > img:nth-child(5)").click
+    reload
+
+    expect(page).to have_selector "#average-rating img[title='gorgeous']"
+  end
+end
