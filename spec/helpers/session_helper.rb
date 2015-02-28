3754d4b1dec1daf30a50b7af8dc9e50bc39b06ae
Jimmy Thelander <jimmy.thelander@gmail.com>
2015-02-28 17:04:16 +0100

    Fix localization of session test helper

--- spec/helpers/session_helper.rb
+++ spec/helpers/session_helper.rb
@@ -2,11 +2,11 @@ module SessionHelper
   def login user
     visit root_path
 
-    click_link "Logga in"
+    click_link I18n.t("navigation.log_in")
     within ".session-form" do
       fill_in "user_email", with: user.email
       fill_in "user_password", with: user.password
-      click_button "Logga in"
+      click_button I18n.t("sessions.submit")
     end
   end
 end
