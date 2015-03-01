da58a7196bac2e8dcc12dcad02d45ebdcd6bc6a5
Jimmy Thelander <jimmy.thelander@gmail.com>
2015-03-01 14:06:55 +0100

    Add Google authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
 
   has_many :ratings
 
