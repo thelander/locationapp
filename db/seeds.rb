ab74791f97f1b7d87937491acbc5066799280e50
Jimmy Thelander <jimmy.thelander@gmail.com>
2015-03-03 19:24:47 +0100

    Create database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+markets = City.create([
+  {
+    name: "Myrorna Sundbyberg",
+    description: "Myrornas butik i Sundbyberg",
+    address: "Sturegatan 36, 172 31 Sundbyberg"
+  },
+  {
+    name: "Myrorna Hötorget",
+    description: "Myrornas butik vid Hötorget",
+    address: "Adolf Fredriks kyrkogata 5, 111 37 Stockholm"
+  },
+  {
+    name: "Myrorna Ropsten",
+    description: "Myrornas butik i Ropsten",
+    address: "Kolargatan 2, 115 42 Stockholm"
+  },
+  {
+    name: "Myrorna Hornsgatan",
+    description: "Myrornas butik på Hornsgatan på Södermalm",
+    address: "Hornsgatan 96, 118 21 Stockholm"
+  },
+  {
+    name: "Myrorna Götgatan",
+    description: "Myrornas butik på Götgatan på Södermalm",
+    address: "Götgatan 79, 116 62 Stockholm"
+  },
+  {
+    name: "Erikshjälpen Second Hand Spånga",
+    description: "Erikshjälpens second hand-butik i Spånga",
+    address: "Skogängsvägen 59, 163 41 Spånga"
+  },
+  {
+    name: "Emmaus Stockholm",
+    description: "Emmaus butik i Stockholm",
+    address: "Peter Myndes backe 8, 118 46 Stockholm"
+  },
+  {
+    name: "Centralantikvariatet",
+    description: "Centralantikvariatet",
+    address: "Österlånggatan 53, 111 31 Stockholm"
+  },
+  {
+    name: "Stockholms Stadsmissions second hand",
+    description: "Myrornas butik i Sundbyberg",
+    address: "Hornsgatan 58, 118 21 Stockholm"
+  }
+])
