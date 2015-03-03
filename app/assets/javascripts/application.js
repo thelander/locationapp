cb656112ad55345e190c79fbfdc6cf010d831ca6
Jimmy Thelander <jimmy.thelander@gmail.com>
2015-03-03 18:45:12 +0100

    Refactor review ratings

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,5 +2,14 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
-//= require star-rating
+//= require bootstrap-rating-input
 //= require_tree .
+
+var ready;
+
+ready = function() {
+  $("#average_rating").rating();
+};
+
+$(document).ready(ready);
+$(document).on("page:load", ready);
