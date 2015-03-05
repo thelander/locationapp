662f6c848668294149fb576aed208204e1f7d07d
Jimmy Thelander <jimmy.thelander@gmail.com>
2015-03-05 13:47:12 +0100

    Use an environment variable for localization

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module LocationApp
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = ENV["LOCATIONAPP_LOCALE"] || :en
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
