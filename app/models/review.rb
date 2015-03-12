701a16c0d7e62aad7faeb2d17a2752c7fc7c3d1f
Jimmy Thelander <jimmy.thelander@gmail.com>
2015-03-12 16:22:19 +0100

    Fix reviews and review feature tests

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,8 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :location
 
-  validates_presence_of :user, :content, :rating
-  validates :content, length: { in: 4..1000 }
+  validates_presence_of :user, :rating
+  validates_inclusion_of :rating, in: 1..5
 
   scope :by_date, -> { order created_at: :desc }
 end
--- spec/features/create_review_spec.rb
+++ spec/features/create_review_spec.rb
@@ -1,24 +1,46 @@
 feature "create review" do
   before do
     user = User.create email: "user@test.com", password: "password"
-    location = Location.create name: "Location", description: "Location description"
+    location = Location.create name: "Location", description: "Description"
 
     login user
     visit location_path location
   end
 
-  scenario "with valid content" do
+  scenario "with content and high rating" do
     within ".review-form" do
-      fill_in :review_content, with: "Review content"
+      fill_in :review_content, with: "Content"
+      fill_in :review_rating, with: 5
+      click_button I18n.t("reviews.submit")
+    end
+
+    expect(page).to have_content I18n.t("reviews.sent")
+    expect(page).to have_content I18n.t("locations.average_rating", average_rating: 5.0, count: 1)
+  end
+
+  scenario "with content and low rating" do
+    within ".review-form" do
+      fill_in :review_content, with: "Content"
+      fill_in :review_rating, with: 1
+      click_button I18n.t("reviews.submit")
+    end
+
+    expect(page).to have_content I18n.t("reviews.sent")
+    expect(page).to have_content I18n.t("locations.average_rating", average_rating: 1.0, count: 1)
+  end
+
+  scenario "without content but with rating" do
+    within ".review-form" do
+      fill_in :review_rating, with: 5
       click_button I18n.t("reviews.submit")
     end
 
     expect(page).to have_content I18n.t("reviews.sent")
   end
 
-  scenario "with invalid content" do
+  scenario "with content but without rating" do
     within ".review-form" do
-      fill_in :review_content, with: "R"
+      fill_in :review_content, with: "Content"
       click_button I18n.t("reviews.submit")
     end
 
